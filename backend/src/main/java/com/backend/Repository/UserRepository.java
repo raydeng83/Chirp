53b71b333fa3d9d86569339e6dc3c7c333ce59b3
Le Deng <z00382545@FreseniusAdmins-MacBook-Pro.local>
2016-11-17 17:09:34 -0500

    add backend

---
+++ backend/src/main/java/com/backend/Repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.backend.Repository;
+
+import com.backend.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ * Created by z00382545 on 11/17/16.
+ */
+
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByUsername(@Param("username") String name);
+}
