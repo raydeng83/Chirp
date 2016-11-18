25ca84724909e6cbb9f8f9705cd33ef82439bf45
Le Deng <z00382545@FreseniusAdmins-MacBook-Pro.local>
2016-11-18 15:06:05 -0500

    add signup

--- frontend/src/components/Signup.vue
+++ frontend/src/components/Signup.vue
@@ -1,56 +1,91 @@
 <template>
-  <form class="form-horizontal" action='' method="POST">
+  <div>
+  <form v-if="!signed" class="form-horizontal" >
     <fieldset>
       <div id="legend">
         <legend class="">Register</legend>
       </div>
       <div class="control-group">
-        <!-- Username -->
-        <label class="control-label"  for="username">Username</label>
+        <label class="control-label" for="username">Username</label>
         <div class="controls">
-          <input type="text" id="username" name="username" placeholder="" class="input-xlarge">
+          <input v-model="user.username" type="text" id="username" name="username" placeholder="" class="input-xlarge">
           <p class="help-block">Username can contain any letters or numbers, without spaces</p>
         </div>
       </div>
       
       <div class="control-group">
-        <!-- E-mail -->
         <label class="control-label" for="email">E-mail</label>
         <div class="controls">
-          <input type="text" id="email" name="email" placeholder="" class="input-xlarge">
+          <input v-model="user.email" type="text" id="email" name="email" placeholder="" class="input-xlarge">
           <p class="help-block">Please provide your E-mail</p>
         </div>
       </div>
 
       <div class="control-group">
-        <!-- Password-->
         <label class="control-label" for="password">Password</label>
         <div class="controls">
-          <input type="password" id="password" name="password" placeholder="" class="input-xlarge">
+          <input v-model="user.password" type="password" id="password" name="password" placeholder="" class="input-xlarge">
           <p class="help-block">Password should be at least 4 characters</p>
         </div>
       </div>
 
-      <div class="control-group">
+      <!-- <div class="control-group">
         <label class="control-label"  for="password_confirm">Password (Confirm)</label>
         <div class="controls">
           <input type="password" id="password_confirm" name="password_confirm" placeholder="" class="input-xlarge">
           <p class="help-block">Please confirm password</p>
         </div>
-      </div>
+      </div> -->
 
       <div class="control-group">
         <!-- Button -->
         <div class="controls">
-          <button class="btn btn-success">Register</button>
+          <a @click="register" class="btn btn-success">Register</a>
         </div>
       </div>
     </fieldset>
   </form>
+
+  <h1 v-if="signed">
+    Thanks for signing up!
+  </h1>
+
+  </div>
 </template>
 
 <script type="text/javascript">
   export default {
-    name: 'sign-up'
+    name: 'sign-up',
+    data () {
+      return {
+        signed: false,
+        user : {
+          username : '',
+          password : '',
+          email : ''
+        }
+      }
+    },
+    methods: {
+      
+        register () {
+          var url = 'http://localhost:8081/user';
+
+          // send post request
+          this.$http.post(url, this.user).then((res) => {
+            console.log(res);
+            this.signed=true;
+          // success callback
+          }, (err) => {
+            console.log(err);
+          // error callback
+          }, {
+            headers: {
+              "Content-Type": "application/json"
+            }
+          });
+        }
+      
+    }
   }
 </script>
